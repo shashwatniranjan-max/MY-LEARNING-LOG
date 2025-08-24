6ccbbc1dffe56c74f05a95f21d3d4ef67b7a3063
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2025-08-24 22:44:27 +0530

    await, reject

--- week2/asyncAwait.js
+++ week2/asyncAwait.js
@@ -47,13 +47,67 @@ function setTimeoutPromisified(ms){
         setTimeout(resolve, ms)
     })
 }
+// setTimeoutPromisified(1000).then(function(){
+//     console.log("hi");
+//     return setTimeoutPromisified(3000)
+// }).then(function(){
+//     console.log("hello");
+//     return setTimeoutPromisified(5000)
+// }).then(function(){
+//     console.log("hey there");
+// })
 
-setTimeoutPromisified(1000).then(function(){
-    console.log("hi");
-    return setTimeoutPromisified(3000)
-}).then(function(){
-    console.log("hello");
-    return setTimeoutPromisified(5000)
-}).then(function(){
-    console.log("hey there");
-})
+// async await syntax
+
+// async function solve(){
+//     await setTimeoutPromisified(1000)
+//     console.log("hi")
+//     await setTimeoutPromisified(3000)
+//     console.log("hello")
+//     await setTimeoutPromisified(5000)
+//     console.log("hey there")
+// }
+// solve();
+
+// const fs = require("fs");
+
+// function readFilePromisified(filePath) {
+//   return new Promise(function (resolve, reject) {
+//     fs.readFile(filePath, "utf-8", function (err, data) {
+//       if (err) {
+//         reject("Error while reading file");
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// }
+
+// function onDone(data) {
+//   console.log(data);
+// }
+
+// function onError(err) {
+//   console.log("Error: " + err);
+// }
+
+// readFilePromisified("a.txt").then(onDone).catch(onError);
+
+
+ const fs = require("fs")
+ function readFilePromisified(filename){
+    return new Promise(function(resolve, reject){
+        fs.readFile(filename, "utf8", function(err, data){
+            if(err){
+                reject("Error while reading this file")
+            } else resolve(data)
+        })
+    })
+ }
+function onData(data){
+    console.log(data)
+}
+function onError(err){
+    console.log("Error" + err)
+}
+readFilePromisified("a.txt").then(onData).catch(onError);
