71e344a88030c30990bb82bd5c531d3502a9489a
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2025-10-14 18:39:35 +0530

    added setTimeout synchronous program in Assignments section

--- Assignments/week2/index.js
+++ Assignments/week2/index.js
@@ -19,24 +19,37 @@
 // const sleep = require("./promise")
 // sleep(5000).then(() => console.log("five seconds have passed"))
 
-function pad(num){
-    return num.toString().padStart(2, "0");
+// function pad(num){
+//     return num.toString().padStart(2, "0");
+// }
+
+// function showTime(){
+//     const date = new Date;
+//     const hours = pad(date.getHours());
+//     const minutes = pad(date.getMinutes());
+//     const seconds = pad(date.getSeconds());
+
+//     const newHours = pad(date.getHours() % 12 || 12);
+//     const ampm = date.getHours >= 12 ? "pm" : "am";
+//     const time24 = `${hours}:${minutes}:${seconds}`;
+//     const time12 = `${newHours}:${minutes}:${seconds}${ampm}`;
+
+//     console.clear();
+//     console.log("24-Hour format ", time24);
+//     console.log("12-Hour format ", time12);
+// }
+// setInterval(showTime, 1000);
+// showTime();
+
+
+// ***** Settimeout synchronous program ******* //
+function setTimeoutSync(ms) {
+    const start = Date.now();
+    while(Date.now() - start !== ms) {
+        
+    }
+    console.log(`${ms/1000} seconds have passed`)
+    return ;
 }
-
-function showTime(){
-    const date = new Date;
-    const hours = pad(date.getHours());
-    const minutes = pad(date.getMinutes());
-    const seconds = pad(date.getSeconds());
-
-    const newHours = pad(date.getHours() % 12 || 12);
-    const ampm = date.getHours >= 12 ? "pm" : "am";
-    const time24 = `${hours}:${minutes}:${seconds}`;
-    const time12 = `${newHours}:${minutes}:${seconds}${ampm}`;
-
-    console.clear();
-    console.log("24-Hour format ", time24);
-    console.log("12-Hour format ", time12);
-}
-setInterval(showTime, 1000);
-showTime();
+setTimeoutSync(5000);
+console.log("hello world")
