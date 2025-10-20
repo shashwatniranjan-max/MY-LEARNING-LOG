4fce8d556929742838b2fca0bcb0cee77bf4a00f
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2025-10-20 23:42:57 +0530

    count digits

--- DSA/index.cpp
+++ DSA/index.cpp
@@ -1,17 +1,27 @@
-// reverse the number
+// // reverse the number
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     int lastdigit;
+    
+//     int num;
+//     cin >> num;
+//     int rev = 0;
+//     while(num > 0) {
+//        lastdigit = num % 10;
+//        rev =  rev * 10 + lastdigit;
+//        num = num / 10; 
+//     }
+//     cout << "reverse number is  " << rev;
+
+// }
+
+// count the number ;
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int lastdigit;
-    
     int num;
     cin >> num;
-    int rev = 0;
-    while(num > 0) {
-       lastdigit = num % 10;
-       rev =  rev * 10 + lastdigit;
-       num = num / 10; 
-    }
-    cout << "reverse number is  " << rev;
-
+    int digits = log10(num) + 1;
+    cout << digits;
 }
