fcb10bbf8163b85d0557e25fac637922a84ca935
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2025-10-20 23:27:27 +0530

    DSA log

---
+++ DSA/index.cpp
@@ -0,0 +1,14 @@
+// reverse the number
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int lastdigit;
+    int num;
+    cin >> num;
+    int rev;
+    while(num > 0) {
+       lastdigit = num % 10;
+       rev =  
+    }
+
+}
