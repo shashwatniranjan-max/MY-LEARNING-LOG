8c5b7ed6703ce49fc3d28e19d97d6ed85557b48c
shashwatniranjan-max <shashwatniranjan1@gmail.com>
2025-10-20 23:35:03 +0530

    first program in dsa log

--- DSA/index.cpp
+++ DSA/index.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 int main() {
     int lastdigit;
+    
     int num;
     cin >> num;
-    int rev;
+    int rev = 0;
     while(num > 0) {
        lastdigit = num % 10;
-       rev =  
+       rev =  rev * 10 + lastdigit;
+       num = num / 10; 
     }
+    cout << "reverse number is  " << rev;
 
 }
